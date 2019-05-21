879df4452d37863c34ff13e00679d60b0b4f326c
Juan Berrocal <juanmaberrocal37@gmail.com>
2019-05-21 09:08:07 -0400

    slack bot integration

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@
 from flask import Flask, jsonify
 
 from app import apartment_scraper
+from app import apartment_bot
 
 app = Flask(__name__)
 
@@ -20,4 +21,20 @@ def slackbot():
 def email():
     resp = jsonify({ 'error': 'Not Authorized', 'success': False })
     resp.status_code = 401
+    return resp
+
+@app.route("/slack-event", methods=['POST'])
+def slack_event():
+    content = request.json
+    
+    if content['type'] == 'url_verification':
+        resp = jsonify({ 'challenge': content['challenge'] })
+        resp.status_code = 200
+    elif content['type'] == 'event_callback':
+        r = apartment_bot.main(content['event'])
+        resp = jsonify(r)
+        resp.status_code = 200
+    else:
+        resp = jsonify({ 'error': 'Bad Request', 'success': False })
+        resp.status_code = 400
     return resp
---
+++ app/apartment_bot.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import requests
+
+from app.config import set_config
+
+EVENT_TYPE = 'app_mention'
+EVENT_TEXT = 'Look for apartments'
+
+SLACK_POST
+
+def main(event):
+    config = set_config()
+    if event['type'] != EVENT_TYPE:
+        return { 'error': 'Invalid event type', 'success': False }
+    if event['text'].find(EVENT_TEXT) == -1:
+        return { 'error': 'Invalid message string', 'success': False }
+
+    url = 'https://slack.com/api/chat.postMessage'
+    data = { 'text': '/apartment-search' }
+    headers = {
+        'Content-type': 'application/json',
+        'Authorization': 'Bearer {}'.format(config['SLACK']['TOKEN'])
+    }
+    return requests.post(url, json=data, headers=headers)
+
+if __name__ == "__main__":
+    main()
